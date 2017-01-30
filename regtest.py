febf10a7749d510ea5139320f0d4d9413f7ba1fb
Pranam <pranam@mit.edu>
2017-01-30 10:43:13 -0500

    Added hidden layer to NN

--- regtest.py
+++ regtest.py
@@ -10,7 +10,10 @@ from sklearn.pipeline import Pipeline
 
 # load dataset
 
-
+#fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+#evaluate model with standardized dataset
 
 # load dataset
 dataframe = pd.read_csv("formatted2.csv", delim_whitespace=False, header=None)
@@ -21,6 +24,18 @@ Y = dataset[:,72]
 
 print X
 
+# Run model directly
+# model = Sequential()
+# model.add(Dense(72, input_dim=72, init='normal', activation='relu'))
+# model.add(Dense(1, init='normal'))
+# # Compile model
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # Fit the model
+# model.fit(X, Y, nb_epoch=150, batch_size=10)
+# # evaluate the model
+# scores = model.evaluate(X, Y)
+# print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
 # define base mode
 def baseline_model():
 	# create model
@@ -30,27 +45,25 @@ def baseline_model():
 	# Compile model
 	model.compile(loss='mean_squared_error', optimizer='adam')
 	return model
-		
 
-# fix random seed for reproducibility
-seed = 7
-# numpy.random.seed(seed)
-# evaluate model with standardized dataset
-# estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=100, batch_size=5, verbose=0)
-# 
-# 
-# kfold = KFold(n_splits=10, random_state=seed)
-# results = cross_val_score(estimator, X, Y, cv=kfold)
-# print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+#Model with 1 hidden layer
+def larger_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(72, input_dim=72, init='normal', activation='relu'))
+	model.add(Dense(6, init='normal', activation='relu'))
+	model.add(Dense(1, init='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model		
 
 
-# evaluate model with standardized dataset
-numpy.random.seed(seed)
-estimators = []
-estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp', KerasRegressor(build_fn=baseline_model, nb_epoch=50, batch_size=5, verbose=0)))
-pipeline = Pipeline(estimators)
+#evaluate model with standardized dataset
+#estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=100, batch_size=5, verbose=0)
+estimator = KerasRegressor(build_fn=larger_model, nb_epoch=100, batch_size=5, verbose=0)
+
 kfold = KFold(n_splits=10, random_state=seed)
-results = cross_val_score(pipeline, X, Y, cv=kfold)
-print("Standardized: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+results = cross_val_score(estimator, X, Y, cv=kfold)
+print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
 
